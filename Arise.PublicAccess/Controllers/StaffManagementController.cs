8cb83057efef9d5d93061f98f4f3963f47a111eb
Mohammad Wasim <mwasim@citi-us.com>
2021-05-21 16:35:50 +0530

    Added AddNewStaff Form In Public Portal

---
+++ Arise.PublicAccess/Controllers/StaffManagementController.cs
@@ -0,0 +1,33 @@
+﻿using Empower.AccessControl;
+using Empower.Common.CacheProviders;
+using Empower.DomainService;
+using Empower.Messaging;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Arise.PublicAccess.Controllers
+{
+    public class StaffManagementController : BaseController
+    {
+        private MessagingService _messagingService;
+
+        public StaffManagementController(MessagingService messagingService, ProviderDomainService domainService,
+            Empower.Logging.ILogger logger, AccessControlManager accessControlManager, ICacheProvider cacheProvider)
+            : base(domainService, logger, accessControlManager, cacheProvider)
+        {
+            _messagingService = messagingService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult AddNewStaff()
+        {
+            return View();
+        }
+    }
+}
