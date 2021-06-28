5fefff36ec95f6af41699fb76adec4f479fb5077
Mohammad Wasim <mwasim@citi-us.com>
2021-06-28 12:12:42 +0530

    Working On Add Class Room

--- Arise.PublicAccess/Controllers/StaffChangeHistoryController.cs
+++ Arise.PublicAccess/Controllers/StaffChangeHistoryController.cs
@@ -10,10 +10,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.Linq.Translations;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-
 namespace Arise.PublicAccess.Controllers
 {
     public class StaffChangeHistoryController :BaseController
@@ -25,7 +22,26 @@ namespace Arise.PublicAccess.Controllers
         }
         public IActionResult Index()
         {
-            return View();
+            StaffChangeLogHistoryViewModel staffChangeLogHistoryViewModel = new StaffChangeLogHistoryViewModel();
+            staffChangeLogHistoryViewModel.ClassIDs = (from f in ProviderDomainService.Repository.PA_AddClassRoom
+                                                       select new SelectListItem
+                                                       {
+                                                           Value = f.ID.ToString(),
+                                                           Text = f.ClassRoomName
+                                                       }).ToList();
+
+            staffChangeLogHistoryViewModel.StaffIDs = (from p in ProviderDomainService.Repository.PA_People
+                                                       join s in ProviderDomainService.Repository.PA_Staffs
+                                                       on p.ID equals s.PersonID
+                                                       join sc in ProviderDomainService.Repository.PA_StaffCharacteristics
+                                                       on s.ID equals sc.StaffID
+                                                       where s.FacilityTypeID == 0
+                                                       select new SelectListItem
+                                                       {
+                                                           Value = p.ID.ToString(),
+                                                           Text = p.FullName
+                                                       }).WithTranslations().ToList();
+            return View(staffChangeLogHistoryViewModel);
         }
         public IActionResult StaffTransfer()
         {
@@ -55,21 +71,35 @@ namespace Arise.PublicAccess.Controllers
             var objStaffData = (from sc in ProviderDomainService.Repository.PA_AddClassRoom
                                      join st in ProviderDomainService.Repository.PA_StaffClassRoom
                                      on sc.ID equals st.ClassRoomID
-                                    select new StaffChangeLogHistoryViewModel{
+                                     where sc.IsDeleted !=true
+                                     select new StaffChangeLogHistoryViewModel{
                                       StaffID =st.StaffID,
-                                      CurrentClassRoomID =sc.ID
-                                     });
+                                      ClassRoomID = sc.ID
+                                     }).ToList();
             return Json(objStaffData.ToDataSourceResult(request));
         }
-        public IActionResult SaveStaff([DataSourceRequest] DataSourceRequest request, PA_StaffChangeLogHistory pA_StaffChangeLogHistory)
+        public IActionResult CreateStaffChangeLog([DataSourceRequest] DataSourceRequest request, PA_StaffChangeLogHistory pA_StaffChangeLogHistory)
         {
-            var staffClassRoomData = ProviderDomainService.Repository.PA_StaffClassRoom.Where(x => x.StaffID == pA_StaffChangeLogHistory.StaffID && x.ClassRoomID == pA_StaffChangeLogHistory.CurrentClassRoomID).FirstOrDefault();
+            var staffClassRoomData = ProviderDomainService.Repository.PA_StaffClassRoom.Where(x => x.StaffID == pA_StaffChangeLogHistory.StaffID && x.ClassRoomID == pA_StaffChangeLogHistory.ClassRoomID).FirstOrDefault();
             staffClassRoomData.ClassRoomID = pA_StaffChangeLogHistory.NewClassRoomID;
+            staffClassRoomData.StaffID = pA_StaffChangeLogHistory.NewStaffID;
             ProviderDomainService.Save();
             pA_StaffChangeLogHistory.CreatedDate = DateTime.Now;
             ProviderDomainService.Repository.Add(pA_StaffChangeLogHistory);
             ProviderDomainService.Save();
             return Json(new[] { pA_StaffChangeLogHistory }.ToDataSourceResult(request));
+        }
+        public IActionResult GetStaffChangeLogHistory([DataSourceRequest] DataSourceRequest request)
+        {
+            var objStaffDataChangeLodData = (from sc in ProviderDomainService.Repository.PA_StaffChangeLogHistory
+                                             select new StaffChangeLogHistoryViewModel
+                                             {
+                                                 ClassRoomID= sc.ClassRoomID,
+                                                 OldStaffID =sc.StaffID,
+                                                 NewStaffID =sc.NewStaffID,
+                                                 CreatedDate =sc.CreatedDate
+                                             }).ToList(); // i have getting errro when i dont used .ToList()
+            return Json(objStaffDataChangeLodData.ToDataSourceResult(request));
         }
     }
 }
